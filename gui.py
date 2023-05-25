e651cb5a870834ff844a93cf1ec9dfa76b3e718e
7ongxia <7ongxia@gmail.com>
2023-05-25 16:29:44 +0800

    changed to jpg -> png
    easy to test

--- gui.py
+++ gui.py
@@ -1,10 +1,8 @@
-import tkinter as tk
-from tkinter import filedialog
-from tkinter.filedialog import askopenfile
-from PIL import Image, ImageTk
-from datetime import datetime
 import boto3
-import os
+import tkinter as tk
+from pathlib import Path
+from datetime import datetime
+from tkinter import filedialog
 
 
 # Set up your AWS credentials and create a new S3 and SQS client object
@@ -15,38 +13,48 @@ region = 'ap-northeast-2'
 s3 = boto3.client('s3', aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key, region_name=region)
 inbox_sqs = boto3.client('sqs', aws_access_key_id=access_key_id,
-                  aws_secret_access_key=secret_access_key, region_name=region)
+                         aws_secret_access_key=secret_access_key, region_name=region)
 inbox_sqs_name = 'c3358_inbox.fifo'
 
 # Create a new S3 bucket or use an existing one
 bucket_name = 'hku-comp3358'
 if s3.head_bucket(Bucket=bucket_name)['ResponseMetadata']['HTTPStatusCode'] != 200:
     s3.create_bucket(Bucket=bucket_name)
-    
+
 window = tk.Tk()
 window.title("Image Resizer")
 window.geometry("640x400+100+100")
 window.resizable(False, False)
 
-b1 = tk.Button(window, text='Upload File', 
-   width=20,command = lambda:upload_file())
-b1.grid(row=1,column=1) 
+b1 = tk.Button(window, text='Upload File',
+               width=20, command=lambda: upload_file())
+b1.grid(row=1, column=1)
+
 
 def upload_file():
     global img
-    f_types = [('Jpg Files', '*.jpg')]
-    img_path = filedialog.askopenfilename(filetypes=f_types) 
-    
-    # Upload img to S3 bucket
-    now = datetime.now()
-    key = now.strftime('%Y-%m-%d-%H-%M-%S')
-    s3.upload_file(img_path, bucket_name, key)
-    
-    # Send a message to the queue
-    queue_url = f'https://sqs.{region}.amazonaws.com/{account_id}/{inbox_sqs_name}'
-    resp = inbox_sqs.send_message(QueueUrl=queue_url, MessageBody=key, MessageDeduplicationId=key, MessageGroupId=key)
-    print(resp)
-    
+    f_types = [('Png Files', '*.png')]
+    img_path = filedialog.askopenfilename(filetypes=f_types)
+
+    if img_path:
+        # Verify the selected file has the expected file type
+        file_extension = Path(img_path).suffix.lower()
+        if file_extension not in ('.jpg', '.jpeg', '.png', '.gif', '.bmp'):
+            print('Invalid file type selected')
+        else:
+            print('Selected file:', img_path)
+            # Upload img to S3 bucket
+            now = datetime.now()
+            key = now.strftime('%Y-%m-%d-%H-%M-%S')
+            s3.upload_file(img_path, bucket_name, key)
+
+            # Send a message to the queue
+            queue_url = f'https://sqs.{region}.amazonaws.com/{account_id}/{inbox_sqs_name}'
+            resp = inbox_sqs.send_message(
+                QueueUrl=queue_url, MessageBody=key, MessageDeduplicationId=key, MessageGroupId=key)
+            print(resp)
+    else:
+        print('No file selected')
 
 
 window.mainloop()
